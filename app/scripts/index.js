e0f1e207683d1fe2cc98cfa7b2e66dbd750a9a04
emermacko <mr.macko.yt@gmail.com>
2021-04-04 00:56:25 +0200

    :sparkles: Mobile version & better post positioning

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -5,48 +5,51 @@ import { Themes } from './obj/theme.js'
 import { saveAs } from 'file-saver'
 import optionIcons from '../img/icons/options/*.svg'
 
-var activePost
+var activePost                  // storing activePost object from postBuilder
 var showingPost = false
 var showingSettings = false
-var showingDate = true
 
 const TWITTER_TOKEN = 'AAAAAAAAAAAAAAAAAAAAALCNNwEAAAAAO0fvQSwiER9X%2FAFxKChvRxgDGYI%3DoWtrE96FRLO8i9nwNxeypKwG9YgCrYWniLg2cVQLTfIqShqTkB'
-const themesTypes = Object.keys(Themes)
-const likesTypes = [
+const themesTypes = Object.keys(Themes)     // storing available theme names from theme.Themes
+const likesTypes = [                        // information about likes options  // outline & filled correspond to icons' filenames
     'disabled',
     'outline',
     'filled'
 ]
-const dateTypes = [
+const dateTypes = [                         // information about date options
     'full',
     'date',
     'disabled'
 ]
+
+/* set defaults */
 themesTypes.active = 0
 likesTypes.active = 0
 dateTypes.active = 0
 
 window.addEventListener('load', () => {
     $('.container').style.opacity = 1
-    $('img').forEach(img => img.draggable = false)
+    $('img').forEach(img => img.draggable = false)      // disable dragging for all images
 
     // todo: reading cookie for activeTheme
 
     console.log('%c Made by: %c https://kazmierczyk.me/', 'background: linear-gradient(to left bottom, #d16ba5, #cf6fb1, #cb73be, #c678cb, #be7ed7, #b388e2, #a692ec, #999bf4, #8ba9f8, #83b5f9, #82c0f6, #89c9f2); color: #fff;', '')
 })
 
-
+/* click event for "LOAD" button at the begininnig */
 $('.load').addEventListener('click', async () => {
-    const input = $('.tweet-input').value
-    let id = input || undefined
-    const split = input.split('/')
+    const input = $('.tweet-input').value   // input node's value
+    let id = input || undefined             // if input is empty set id to undefined otherwise to its value
+    const split = input.split('/')          // check if full URL was provided, if yes extract id and set variable
     if(split.length > 1) {
         id = split[split.length-1]
     }
 
-    $('.load').classList.add('dots')
-    const results = await getTweetInformation(id)
-    const tweetData = parseTweetInformation(results)
+    $('.load').classList.add('dots')                    // start loading animation
+    const results = await getTweetInformation(id)       // fetch tweet information
+    const tweetData = parseTweetInformation(results)    // parse tweet information
+
+    /* wait 0.5s and show post */
     setTimeout(() => {
         $('.load').classList.remove('dots')
         $('.input-overlay').style.top = -window.innerHeight-200
@@ -56,81 +59,90 @@ $('.load').addEventListener('click', async () => {
     showTweet(tweetData)
 })
 
+/* event for top arrow to restore post input */
 $('.top-arrow').addEventListener('click', () => {
     $('.input-overlay').style.top = 0
     showingPost = false
 })
 
-$('body').addEventListener('click', () => {
-    if(showingSettings) {
-        hideSettings();
-    }
-})
-
+/* binding events to all options */
 $('.option').forEach(option => {
-    const type = option.id.replace('option-', '')
-    option.addEventListener('click', function(e) {
+    const type = option.id.replace('option-', '')       // extract option type from id
+    option.addEventListener('click', function() {       // add event depending on type
         switch(type) {
+
+            /* event for theme switching */
             case 'themes':
-                themesTypes.active = themesTypes.active === themesTypes.length-1 ? 0 : themesTypes.active + 1
-                const newTheme = themesTypes[themesTypes.active]
-                activePost.applyTheme(Themes[newTheme])
-                $('.option-themes > img').src = optionIcons[`theme-${newTheme.toLowerCase()}`]
+                themesTypes.active = themesTypes.active === themesTypes.length-1 ? 0 : themesTypes.active + 1   // increment or reset index
+                const newTheme = themesTypes[themesTypes.active]                                                // save new theme name
+                activePost.applyTheme(Themes[newTheme])                                                         // apply new theme
+                $('.option-themes > img').src = optionIcons[`theme-${newTheme.toLowerCase()}`]                  // change brush icon based on selected theme
                 break
 
+            /* event for datetime style switching */
             case 'datetime':
-                dateTypes.active = dateTypes.active === dateTypes.length-1 ? 0 : dateTypes.active + 1
-                const dateType = dateTypes[dateTypes.active]
+                dateTypes.active = dateTypes.active === dateTypes.length-1 ? 0 : dateTypes.active + 1           // increment or reset index
+                const dateType = dateTypes[dateTypes.active]                                                    // save datetime type
+
+                /* if datetime should be hidden */
                 if(dateType === 'disabled') {
-                    $('.datetime').fadeOut(300, true)
-                    this.classList.add('option-disabled')
+                    $('.datetime').fadeOut(300, true)          // fadeout actual date
+                    this.classList.add('option-disabled')      // set icon style to disabled
                     break
-                }
-                this.classList.remove('option-disabled')
+                } this.classList.remove('option-disabled')     // otherwise remove disabled style
+
+                /* if datetime should be fully shown */
                 if(dateType === 'full') {
+                    /* load all parts */
                     $('.datetime').querySelectorAll('span').forEach(el => {
                         el.fadeIn(1, 'inline')
                     })
-                    $('.datetime').fadeIn(300, 'block')
+                    $('.datetime').fadeIn(300, 'block') // fade in container
                     break
                 }
+
+                /* if datetime should only show date */
                 if(dateType === 'date') {
+                    /* fade out all remaining elements */
                     $('.datetime').querySelectorAll('span:not(.datetime-date)').forEach(el => {
                         el.fadeOut(300, true)
                     })
                 }
                 break
 
+            /* event for likes style switching */
             case 'likes':
-                likesTypes.active = likesTypes.active === likesTypes.length-1 ? 0 : likesTypes.active + 1
-                if(likesTypes[likesTypes.active] === 'disabled') {
-                    $('.option-likes > img').src = optionIcons[`heart-outline`]
-                    this.classList.add('option-disabled')
-                    $('.likes').fadeOut(300, true)
+                likesTypes.active = likesTypes.active === likesTypes.length-1 ? 0 : likesTypes.active + 1           // increment or reset index
+                const newHeart = likesTypes[likesTypes.active]                                                      // save new likes style
+
+                /* if likes hould be hidden */
+                if(newHeart === 'disabled') {
+                    $('.option-likes > img').src = optionIcons[`heart-outline`]     // set option icon to outline
+                    this.classList.add('option-disabled')                           // set icon style to disabled
+                    $('.likes').fadeOut(300, true)                                  // fade out actual likes amount
                     break
                 }
+
+                /* check if likes are hidden & fade them back */
                 if(this.classList.contains('option-disabled')) {
                     this.classList.remove('option-disabled')
                     $('.likes').fadeIn(300, 'flex')
                 }
-                const newHeart = likesTypes[likesTypes.active]
+                /* set icons' style according to selected type */
                 $('.likes > img').src = optionIcons[`heart-${newHeart}`]
                 $('.option-likes > img').src = optionIcons[`heart-${newHeart}`]
-
                 break
-        }
-        e.stopPropagation()
-        
+        }        
     })
 })
 
-$('.settings').addEventListener('click', (e) => {
+/* show or hide settings on click */
+$('.settings').addEventListener('click', () => {
     if(showingSettings) {
         hideSettings()
     } else {
         showSettings()
     }
-    e.stopPropagation();
 })
 
 function hideSettings() {
@@ -161,19 +173,20 @@ function showSettings() {
 }
 
 function showTweet(data) {
-    activePost = new PostBuilder(data.tweet, data.user, data.media, Themes.WHITE)
-    activePost.display()
-    showingPost = true
+    activePost = new PostBuilder(data.tweet, data.user, data.media, Themes[themesTypes[themesTypes.active]])   // create new post
+    activePost.display()    // show it
+    showingPost = true      // set flag
 }
 
 function parseTweetInformation(data) {
+    /* if ID is empty // currently overriden */
     if(data === undefined) {
         throw new Error('No post provided')
     }
-    const tweet_data = data.data
+    const tweet_data = data.data    // save tweet information
 
-    const twDateArray = new Date(tweet_data.created_at).toString().split(" GMT")[0].split(' ')   // format ISO 8601 date
-    const twTimeArray = twDateArray[twDateArray.length - 1].split(':')                          // separate time from date
+    const twDateArray = new Date(tweet_data.created_at).toString().split(" GMT")[0].split(' ')   // split ISO 8601 date to array
+    const twTimeArray = twDateArray[twDateArray.length - 1].split(':')                           // separate time from date
     const tweetDate = {
         full: twDateArray.join(' '),
         dayName: twDateArray[0],
@@ -188,34 +201,37 @@ function parseTweetInformation(data) {
         id: tweet_data.id,
         likes: tweet_data.public_metrics.like_count,
         date: tweetDate,
-        text: tweet_data.text.split(' ').filter(el => !el.includes('t.co')).join(' ')
+        text: tweet_data.text.split(' ').filter(el => !el.includes('t.co')).join(' ')   // remove t.co/id link from tweet text
     }
-    const user = data.includes.users[0]
-    const media = data.includes.media
+    const user = data.includes.users[0]     // save user information
+    const media = data.includes.media       // save media information
 
     return {tweet: tweet, user: user, media:media}
 }
 
 async function getTweetInformation(id = '1343331784621256709') {
+    /* if ID is empty // currently overriden */
     if(id === undefined) {
         return undefined
     }
 
-    const endpointURL = 'https://api.twitter.com/2/tweets/'
-    const prefix = 'https://cors.bridged.cc/'
+    const endpointURL = 'https://api.twitter.com/2/tweets/'     // api endpoint
+    const prefix = 'https://cors.bridged.cc/'                   // CORS proxy server
     const params = {
-        "tweet.fields": "created_at,author_id,public_metrics",
-        "expansions": "author_id,attachments.media_keys",
-        "user.fields": "created_at,profile_image_url,username,verified,name",
-        "media.fields": "url,preview_image_url"
+        "tweet.fields": "created_at,author_id,public_metrics",                  // tweet parameters
+        "expansions": "author_id,attachments.media_keys",                       // additional fields
+        "user.fields": "created_at,profile_image_url,username,verified,name",   // user parameters
+        "media.fields": "url,preview_image_url,height,width"                    // media parameters
     }
 
+    /* build params string to URL-like format ...?param=val&param2=val2 */
     let combinedParams = '';
     for(let p in params) {
         combinedParams += (combinedParams === '') ? '?' : '&'
         combinedParams += `${p}=${params[p]}`
     }
 
+    /* make request and return response */
     const requestURL = `${prefix}${endpointURL}${id}${combinedParams}`
     const res = await fetch(requestURL, {
         headers: {
@@ -228,29 +244,41 @@ async function getTweetInformation(id = '1343331784621256709') {
     return res
 }
 
-function loadThemes() {
-    const themeContainer = $('.theme-container')
-    for(let [name, theme] of Object.entries(Themes)) {
-        const div = document.createElement('div')
-            div.classList.add('theme')
-            div.classList.add(name)
-            div.dataset.color = theme.background
-            div.style.backgroundColor = theme.background
-            div.addEventListener('click', function() {
-                updateActiveTheme(name)
-                activePost.applyTheme(theme)
-            })
-        themeContainer.appendChild(div)
-    }
-}
+/* function for creating style divs // currently not used */
+// function loadThemes() {
+//     const themeContainer = $('.theme-container')
+//     for(let [name, theme] of Object.entries(Themes)) {
+//         const div = document.createElement('div')
+//             div.classList.add('theme')
+//             div.classList.add(name)
+//             div.dataset.color = theme.background
+//             div.style.backgroundColor = theme.background
+//             div.addEventListener('click', function() {
+//                 activePost.applyTheme(theme)
+//             })
+//         themeContainer.appendChild(div)
+//     }
+// }
 
-export async function createScreenshot(sourceNode, targetNode, download=false) {
+/* temporary option to actually save screenshot */
+$('.avatar').addEventListener('click', () => {
+    activePost.createPhoto()
+})
+
+export async function createScreenshot(sourceNode, targetNode, download=true) {
     if(download) {
-        // await dom2Img.toBlob(sourceNode).then(function (blob) {
-        //     window.saveAs(blob, 'my-node.png');
-        // }).catch(function (error) {
-        //     console.error('oops, something went wrong!', error);
-        // })
+        await dom2Img.toBlob(sourceNode, {
+            /* temporary override style to avoid weird cropping */
+            style: {
+                marginTop: 0,
+                top: 0,
+                position: 'unset',                
+            }
+        }).then(function (blob) {
+            window.saveAs(blob, 'my-node.png'); // probably "author_name + date" in the future
+        }).catch(function (error) {
+            console.error('Something went wrong!: ', error);
+        })
     } else {
         await dom2Img.toPng(sourceNode).then(function(dataUrl) {
             const img = document.createElement('img')
@@ -267,7 +295,11 @@ export async function createScreenshot(sourceNode, targetNode, download=false) {
 
 window.addEventListener('resize', () => {
     if(showingPost) {
-        $('.input-overlay').style.top = -window.innerHeight-200
+        $('.input-overlay').style.top = -window.innerHeight-200     // in case of vertical resize keep overlay above
+        
+        /* position post container */
+        $('.post-container').style[isDesktop() ? 'top' : 'marginTop'] = activePost.getPostTop()
+        $('.post-container').style[!isDesktop() ? 'top' : 'marginTop'] = ''
     }
 })
 
--- app/scripts/obj/postBuilder.js
+++ app/scripts/obj/postBuilder.js
@@ -1,32 +1,52 @@
-import {$} from '../common.js'
-import {createScreenshot, positionSettings} from '../index.js'
+import {$, isDesktop} from '../common.js'
+import {createScreenshot} from '../index.js'
+
+/*
+    class for creating, displaying and managing post
+*/
+
 
 export class PostBuilder {
+
     constructor(tweet, user, media, theme) {
         this.tweet = tweet
         this.user = user
         this.media = media
         this.theme = theme
+        this.top    // storing later calculated post top/margin-top value
     }
 
+    /* create screenshot */
     async createPhoto() {
-        const post = $('.post-container')
-        const tmp = post.cloneNode(true)
-        
-        post.querySelectorAll('.media').forEach(el => {
-            if(el.classList.contains('hidden')) {
-                el.remove()
+        const post = $('.post-container')      
+
+        /* hide all elements that should be ignored */ 
+        post.querySelectorAll('*').forEach(el => {
+            if(el.classList.contains('ignore')) {
+                el.style.display = 'none'
             }
         })
+    
+        await createScreenshot(post, $('.content'))     // save screenshot
 
-        await createScreenshot(post, $('.content'), true)
-        $('.content').replaceChild(tmp, post)
+        /* restore ignored elements */
+        post.querySelectorAll('*').forEach(el => {
+            if(el.classList.contains('ignore')) {
+                el.style.display = ''
+            }
+        })
     }
 
+    /* show post */
     display() {
         const date = this.tweet.date
         const likes = this.tweet.likes
 
+        const multiMediaHeight = 0.4 * window.innerHeight       // media-2/3/4 photos height (40vh)
+        const croppedMaxHeight = 0.55 * window.innerHeight      // media-1 cropped photo height limit (55vh)
+        const postPadding = 20                                  // default post padding (20px)
+
+        /* fill in all post's information */
         $('.post-container').style.opacity = 1
         $('.avatar').src = this.user.profile_image_url.replace('_normal', '')
         $('.name').innerHTML = this.user.name
@@ -34,40 +54,67 @@ export class PostBuilder {
         $('.text').innerHTML = this.tweet.text
         $('.datetime-time').innerHTML = `${date.hours}:${date.minutes}`
         $('.datetime-date').innerHTML = `${date.day} ${date.month} ${date.year}`
-        $('.likes-amount').innerHTML = likes >= 1000 ? `${parseFloat(likes/1000.0).toFixed(1)}k` : likes
+        $('.likes-amount').innerHTML = likes >= 1000 ? `${parseFloat(likes/1000.0).toFixed(1)}k` : likes    // format likes with 'k' suffix if more than 1000
 
         this.applyTheme(this.theme)
 
+        /* hide all media divs */
         $('.media').forEach(el => {
             el.classList.add('hidden')
         })
 
+        /* fill in & display correct media div */
         if(this.media !== undefined) {
             $('.text').classList.remove('nomedia')
 
-            const amount = this.media.length
-            const nr = amount <= 4 ? amount : 'multi'
-            const mediaContainer = $(`.media-${nr}`)
-                mediaContainer.classList.remove('hidden')
+            var actualHeight                                    // variable to store pre-calculated post height (before the photos are downloaded, based on api dimensions information)
+            const amount = this.media.length                    // count how many media files are in post
+            const mediaContainer = $(`.media-${amount}`)        // select container based on media amount
+                mediaContainer.classList.remove('hidden')       // unhide it
 
+            /* if only one media post */
             if(amount === 1) {
-                mediaContainer.querySelector('img').src = this.media[0].url
-            } else {
+                const whRatio = this.media[0].width / this.media[0].height                  // calculate width to height ratio based on API information
+                const postInnerWidth = $('.post-container').offsetWidth - postPadding*2     // get post-container media space width
+
+                actualHeight = postInnerWidth / whRatio     // calculate predicted photo height from post width and w/h ratio
+                if($('.media-1 > img').classList.contains('cropped') && actualHeight > croppedMaxHeight) {      // if "cropped" option is selected re-calculate height if needed
+                    actualHeight = croppedMaxHeight
+                }
+
+                mediaContainer.querySelector('img').src = ''    // reset photo url (needed when using top-arrow to re-enter post)
+                mediaContainer.querySelector('img').src = (this.media[0].type === 'photo') ? this.media[0].url : this.media[0].preview_image_url    // set either photo or video thumnail as post src
+            } else {    // case for 2-4 media files
                 let counter = 0
+
+                /* load fetched images as background images */
                 mediaContainer.querySelectorAll('.media-content').forEach(el => {
-                    el.style.backgroundImage = `url(${this.media[counter++].url})`
+                    const image = this.media.type === 'photo' ? this.media[counter].url : this.media[counter].preview_image_url
+                    tmp.src = image
+                    el.style.backgroundImage = `url('')`
+                    el.style.backgroundImage = `url(${image})`
+                    counter++
                 })
+                actualHeight = multiMediaHeight     // set media height to multiMedia CSS height value
             }
         } else {
             $('.text').classList.add('nomedia')
         }
 
-        $('.settings-container').style.opacity = 1
+        actualHeight += $('.post-container').offsetHeight   // increment height by post height
+        this.top = (window.innerHeight - actualHeight) / 2     // calculate center for post
+
+        $('.post-container').style[isDesktop() ? 'top' : 'marginTop'] = this.top   // set post position
+        $('.settings-container').style.opacity = 1      // fade in settings container
     }
 
     applyTheme(theme) {
         $('.post-container').style.backgroundColor = theme.background
         $('.text-primary').forEach(el => el.style.color = theme.text.primary)
         $('.text-secondary').forEach(el => el.style.color = theme.text.secondary)
+    }
+
+    getPostTop() {
+        return this.top
     }
 }
